272a77b887de7bfefcaf57fac247c94ac3bf9449
jvrs2812 <jvrs_souza@hotmail.com>
2021-11-20 19:46:19 -0300

    adicionando sqlite

--- backend/src/ProEventos.API/Controllers/EventoController.cs
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,60 +13,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]{
-                new Evento(){
-                  EventoId = 1,
-                  Tema = "angular 11 e .NET 5",
-                  Local = "Belo Horizonte",
-                  Lote = "1 lote",
-                  QtdPessoas = 250,
-                  DataEvento = DateTime.Now.AddDays(2).ToString(),
-                  ImageURL = "foto.png",
-                },
-                new Evento(){
-                  EventoId = 2,
-                  Tema = "teste",
-                  Local = "Belo Horizonte",
-                  Lote = "1 lote",
-                  QtdPessoas = 250,
-                  DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
-                  ImageURL = "foto.png",
-                },
-                new Evento(){
-                  EventoId = 3,
-                  Tema = "lote",
-                  Local = "Belo Horizonte",
-                  Lote = "1 lote",
-                  QtdPessoas = 250,
-                  DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
-                  ImageURL = "foto.png",
-                },
-                new Evento(){
-                  EventoId = 4,
-                  Tema = "aqeqweq 5",
-                  Local = "Belo Horizonte",
-                  Lote = "1 lote",
-                  QtdPessoas = 250,
-                  DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
-                  ImageURL = "foto.png",
-                }
-        };
+        private readonly DataContext _context;
 
-        public EventoController()
+        public EventoController(DataContext context)
         {
-    
+            this._context = context;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
     }
 }
